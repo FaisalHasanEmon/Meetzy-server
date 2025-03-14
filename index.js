bb4eb30b869bbd0210c6772a790161ecd2902be0
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-03-14 14:25:32 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/checking-meetzy", (req, res) => {
+  res.send({ key: "I am working for meetzy" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
