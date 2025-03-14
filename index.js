86b0cc75fbbb8700e2b560e32c843cc27c93012c
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-03-14 14:28:49 +0600

    Faisal: development branch intialized

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/checking-meetzy", (req, res) => {
+app.get("/meetzy", (req, res) => {
   res.send({ key: "I am working for meetzy" });
 });
 
